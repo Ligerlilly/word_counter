c5c2540f38f08c053b3ddad6f0d61722686d3758
Jason <quizathaderat@yahoo.com>
2015-08-07 09:26:19 -0700

    Add test for case insensitivity

--- spec/word_conter_spec.rb
+++ spec/word_conter_spec.rb
@@ -5,4 +5,8 @@ describe 'String#word_counter' do
   it 'takes a string as an argument and returns number of instances of the argument in string object' do
     expect("Hi there".word_counter('Hi')).to eq(1)
   end
+
+  it 'is case insensitive' do
+    expect('Hi there'.word_counter('hi')).to eq(1)
+  end
 end
