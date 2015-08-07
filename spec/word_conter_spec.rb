404127809030862fa3f5300b6709f9d378568e10
Jason <quizathaderat@yahoo.com>
2015-08-07 09:33:01 -0700

    Add test for long strings; passes

--- spec/word_conter_spec.rb
+++ spec/word_conter_spec.rb
@@ -9,4 +9,8 @@ describe 'String#word_counter' do
   it 'is case insensitive' do
     expect('Hi there'.word_counter('hi')).to eq(1)
   end
+
+  it 'works with long strings' do
+    expect("If Peter Piper picked a peck of pickled peppers, where's the peck of pickled peppers Peter Piper picked?".word_counter('peck')).to eq(2)
+  end
 end
