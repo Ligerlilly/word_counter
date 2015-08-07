539af822fc4a32becbf23d200a64214878f1c219
Jason <quizathaderat@yahoo.com>
2015-08-07 12:22:29 -0700

    Install body-parser

--- word_counter_js/app.js
+++ word_counter_js/app.js
@@ -1,8 +1,19 @@
  var express = require('express');
  var app = express();
+ var exphbs = require('express3-handlebars');
+ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+ app.set('view engine', 'handlebars');
+ app.use(bodyParser.urlencoded({ extended: true }));
+
+ app.use('/public', express.static('public'));
 
  app.get('/', function(request, response){
-   response.send("hello world");
+   response.render("index");
+ });
+
+ app.get('/word_count', function(request, response){
+   console.log(request.params.body);
+   response.render('word_count');
  });
 
  app.listen(4567, function(request, response){
